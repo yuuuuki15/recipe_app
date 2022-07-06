8707f07929f811db4ffa771a42898699dcd413a0
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-07-06 12:03:45 +0900

    ビューの変更
    トップページにナビバーを追加（bootstrap)

--- db/migrate/20220704082037_devise_create_users.rb
+++ db/migrate/20220704082037_devise_create_users.rb
@@ -4,9 +4,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,               null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :name, null: false
 
       ## Recoverable
       t.string   :reset_password_token
