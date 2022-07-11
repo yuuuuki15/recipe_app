2fd7f000b403c61ca71a5174d235468f121bffd5
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-07-11 12:11:28 +0900

    preview.js実装

---
+++ app/javascript/preview.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const postForm = document.getElementById('new_post');
+  const previewList = document.getElementById('previews');
+
+  if (!postForm) return null;
+
+  const fileField = document.querySelector('input[type="file"], [name="recipe[image]"]');
+  fileField.addEventListener('change', function(e){
+    const alreadyPreviewed = previewList.querySelector('.preview');
+    if(alreadyPreviewed){
+      alreadyPreviewed.remove();
+    };
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+    console.log(blob);
+
+    const previewWrapper = document.createElement('div');
+    previewWrapper.setAttribute('class', 'preview');
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('class', 'preview-image');
+    previewImage.setAttribute('src', blob);
+    previewWrapper.appendChild(previewImage);
+    previewList.appendChild(previewWrapper);
+  });
+});
