2f7e625cfb45ab80a6cc9861e04ae4a4e260c2bb
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-07-13 14:45:03 +0900

    献立一覧表示実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,16 @@ class UsersController < ApplicationController
     wdays = ['(月)','(火)','(水)','(木)','(金)','(土)','(日)']
     @first_day = Date.today.beginning_of_week
 
+    menus = Menu.where(user_id: @user.id, date: @first_day..@first_day + 6)
+
     @week_days = []
     7.times do |x|
+      today_menus = []
+      menus.each do |menu|
+        today_menus.push(menu.recipe.title) if menu.date == @first_day + x
+      end
       day = @first_day + (x - 1)
-      days = { wday: wdays[day.wday], month: day.month, date: day.day }
+      days = { wday: wdays[day.wday], month: day.month, date: day.day, menu: today_menus }
       @week_days.push(days)
     end
   end
