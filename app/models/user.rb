49e099f6e8d79348ce804a487d4642da6a319193
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-07-14 20:08:21 +0900

    commentsテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :recipes, dependent: :destroy
   has_many :menus, dependent: :destroy
   has_many :lists, dependent: :destroy
+  has_many :comments
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
