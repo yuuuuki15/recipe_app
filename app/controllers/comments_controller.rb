0e745124457a23ca45ccb9b7f2a9997b60117906
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-07-15 13:39:17 +0900

    コメント削除機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,6 +13,13 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    flash[:notice] = 'コメントを削除しました'
+    redirect_to recipe_path(@comment.recipe)
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id, recipe_id: @recipe.id)
