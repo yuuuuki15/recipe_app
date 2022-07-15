4fd761654d18db32dd5929da9965d476caf3f981
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-07-15 18:39:41 +0900

    ルーティングとコントローラーの実装

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,4 +3,19 @@ class Relationship < ApplicationRecord
   belongs_to :followed, class_name: 'User'
   has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
   has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人
+
+  # ユーザーをフォローする
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  # フォローしていればtrueを返す
+  def following?(user)
+    following_user.include?(user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
+  resources :users, only:[:index, :show, :edit, :update] do
+    member do
+      get :follows, :followers
+    end
+    resource :relationships, only: [:create, :destroy]
+  end
   root 'recipes#index'
   resources :recipes do
     resources :menus, only: [:create]
