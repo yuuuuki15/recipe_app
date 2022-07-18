d13e09ffc13858bc0d4a0c334e13aedb8942fbf0
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-07-18 16:55:02 +0900

    デプロイ自動化

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '35.74.89.131', user: 'ec2-user', roles: %w{app db web}
+server '54.249.3.49', user: 'ec2-user', roles: %w{app db web}
