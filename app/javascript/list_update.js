bf91688404c7e69b846a136c699f479ee6f0b20a
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-07-21 16:57:59 +0900

    レシピ詳細手順のoverflow:scroll

--- app/javascript/list_update.js
+++ app/javascript/list_update.js
@@ -2,10 +2,13 @@ window.addEventListener('load', function() {
   const updateList = document.querySelectorAll('#update');
   for (let i = 0; i < updateList.length; i++) {
   updateList[i].addEventListener('click', function(e) {
-    const formData = new FormData();
-    const listId = updateList[i].childNodes
-    //formdataをllistIdに追加
-    formData.append('listId', listId);
+    const list = e.target
+    const new_element = document.createElement('p');
+    new_element.textContent = '追加テキスト';
+    /*listにnew_elementを重ねる 
+    list.appendChild(new_element);
+    const formData = new FormData(e.target);*/
+
     /*
     const editListId = document.querySelectorAll('.edit-list-form')
       XHR = new XMLHttpRequest();
