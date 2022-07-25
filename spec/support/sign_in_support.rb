7ec92e02e3ecb866d4a25183e670078c85bd96f8
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-07-25 12:36:40 +0900

    basic_passをモジュールにする

--- spec/support/sign_in_support.rb
+++ spec/support/sign_in_support.rb
@@ -9,4 +9,10 @@ module SignInSupport
     find("input[name='commit']").click
     expect(current_path).to eq(root_path)
   end
+
+  def basic_pass(path)
+    username = ENV["BASIC_AUTH_USER"]
+    password = ENV["BASIC_AUTH_PASSWORD"]
+    visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{path}"
+  end
 end
--- spec/system/recipes_spec.rb
+++ spec/system/recipes_spec.rb
@@ -1,9 +1,4 @@
 require 'rails_helper'
-def basic_pass(path)
-  username = ENV["BASIC_AUTH_USER"]
-  password = ENV["BASIC_AUTH_PASSWORD"]
-  visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{path}"
-end
 
 RSpec.describe "Recipes", type: :system do
   before do
