3029a32f639350ac5430b685d794dd5d55795b8a
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-08-01 14:27:17 +0900

    レダイレクト先変更
    アニメーション一時中止して、
    献立を追加したときのリダイレクト先をマイページに変更することで、献立がどこに追加されたか分かりやすくする。

--- app/javascript/calender.js
+++ app/javascript/calender.js
@@ -9,6 +9,21 @@ window.addEventListener('load', function() {
       calender.removeAttribute("style", "display:block;");
     }else{
     calender.setAttribute("style", "display:block;");
-    }
+    };
   });
+
+  // const calender_submit_button = document.getElementById('calender-submit-button');
+  // calender_submit_button.addEventListener('click', function(e) {
+    // form = document.getElementById('calender-form');
+    // const formData = new FormData(form);
+    // e.preventDefault();
+    // const XHR = new XMLHttpRequest();
+    // XHR.open('POST', '/recipes/menus', true);
+    // XHR.responseType = 'json';
+    // XHR.send(formData);
+    // XHR.onload = function() {
+      // const animation = document.getElementsByClassName('header-mypage');
+      // animation.className = 'active';
+    // };
+  // });
 });
