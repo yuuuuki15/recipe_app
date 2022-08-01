af4596517ebe93fc84826f94758f9a38d9079518
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-08-01 20:27:35 +0900

    一般公開ボタンをわかりやすく実装

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
   const postForm = document.getElementById('new_post');
   const previewList = document.getElementById('previews');
 
+
   //if (!postForm) return null;
 
   const fileField = document.querySelector('input[type="file"], [name="recipe[image]"]');
